dab611fead0c38f6e384e8b4ae90e45defa77928
Marcelo Castañeda III <marcelocastanedaiii@gmail.com>
2024-01-15 16:45:04 +0800

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        dark:'#323232',
+        light:'#F1F0EE',
+        primary:'#309340',
+        secondary: '#4D772D',
+        grey: '#DCD9D2',
+      },
+  
+    },
+  },  
+  plugins: [],
+}
+
